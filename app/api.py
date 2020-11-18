4b1b1da7eb48147e79402fc22f616f544e6144f4
jovenium <lucas1789l@gmail.com>
2020-11-18 15:47:34 +0100

    ajout de l'accueil dans templates

--- app/api.py
+++ app/api.py
@@ -1,84 +1,82 @@
-# Product Service
+#################################
+#~~~~~~ App ENSIIE- NOSQL ~~~~~~# 
+#################################
 
 # Import framework
-from flask import Flask
-from flask_restful import Resource, Api
-import redis
+from flask import Flask,render_template	
+#from flask_restful import Resource, Api 
+import redis as RedisD
 import psycopg2
 from neo4j import GraphDatabase
 from pymongo import MongoClient
 
-# Instantiate the app
+# initialisation de l'application
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
+@app.route('/')
+def home():
+		return render_template("home.html")
 
-class Redis(Resource):
-	def get(self):
-		r = redis.Redis(host='redis',port='6379',db=0)
-		try:
-			if r.ping():
-				r.incr('compteur')
-				compteur = r.get('compteur')	
-				return {
-				'redis': ['compteur:', str(compteur)]
-				}
-		except:
+@app.route('/redis')
+def redis():
+	r = RedisD.Redis(host='redis',port='6379',db=0)
+	try:
+		if r.ping():
+			r.incr('compteur')
+			compteur = r.get('compteur')	
 			return {
-				'fails': ['fails', 'fails', 'fails', 'fails']
+			'redis': ['compteur:', str(compteur)]
 			}
+	except:
+		return {
+			'redis': ['fail']
+		}
 			
+@app.route('/psql')
+def postgres():
+	try:
+		conn = psycopg2.connect(
+			host="psql",    #nom du service compose
+			database="postgres",
+			user="postgres",
+			password="postgres",
+			port='5432')
+		
+		return {
+		'psql': ['connexion:', 'OK']
+		}
+	except:
+		return {
+			'psql': ['fail']
+		}
 			
-class Postgres(Resource):
-	def get(self):
-		try:
-			conn = psycopg2.connect(
-				host="psql",    #nom du service compose
-				database="postgres",
-				user="postgres",
-				password="postgres",
-				port='5432')
+@app.route('/neo4j')
+def neo4j():
+	try:
+		conn_neo = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "neo4j"))
+		
+		return {
+		'neo4j': ['connexion:', 'OK']
+		}
+	except:
+		return {
+			'neo4j': ['fail']
+		}
 			
-			return {
-			'psql': ['ping:', 'pong']
-			}
-		except:
-			return {
-				'psql': ['fail']
-			}
-			
-class Neo4j(Resource):
-	def get(self):
-		try:
-			conn_neo = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "neo4j"))
-			
-			return {
-			'neo': ['4j', 'OK']
-			}
-		except:
-			return {
-				'neo': ['fail']
-			}
-			
-class Mongo(Resource):
-	def get(self):
-		try:
-			client = MongoClient('mongodb://mongo:mongo@mongo')
-			
-			return {
-			'mongo': ['ognom']
-			}
-		except:
-			return {
-				'mongo': ['nop']
-			}
+@app.route('/mongo')
+def mongo():
+	try:
+		client = MongoClient('mongodb://mongo:mongo@mongo')
+		
+		return {
+		'mongo': ['connexion:', 'OK']
+		}
+	except:
+		return {
+			'mongo': ['fail']
+		}
 
-# Create routes
-api.add_resource(Redis, '/')
-api.add_resource(Postgres, '/psql')
-api.add_resource(Neo4j, '/neo4j')
-api.add_resource(Mongo, '/mongo')
-
-# Run the application
+# Lancement de l'application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
