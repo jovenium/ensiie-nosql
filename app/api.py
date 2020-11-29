fc49038ceb5b7298d115e1c2bc7e4c8b14f133fb
lucas.vauterin <lucas.vauterin@ensiie.fr>
2020-11-29 23:49:58 +0100

    login, logout, createPostIt, session

--- app/api.py
+++ app/api.py
@@ -3,7 +3,9 @@
 #################################
 
 # Import framework
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect, url_for, session
+import logging
+import os
 #from flask_restful import Resource, Api 
 import redis as RedisD
 import psycopg2
@@ -12,7 +14,11 @@ from pymongo import MongoClient
 
 # initialisation de l'application
 app = Flask(__name__)
-#api = Api(app)
+databases= {1: 'neo4j', 2: 'mongodb', 3: 'postgres'}
+conn_neo = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "admin"))
+conn_red = RedisD.Redis(host='redis',port='6379',db=0)
+conn_mon = MongoClient('mongodb://mongo:mongo@mongo')
+conn_psq = psycopg2.connect(host="psql",database="postgres",user="postgres",password="postgres",port='5432')
 
 def redis_increment():
 	r = RedisD.Redis(host='redis',port='6379',db=0)
@@ -25,22 +31,70 @@ def redis_increment():
 
 @app.route('/')
 def home():
+		if session.get('logged_in') :
+			return redirect(url_for('todo',name = session.get('todo')))
+		return redirect(url_for('login'))
+
+@app.route('/<name>')
+def todo(name):
+		if not session.get('logged_in') :
+			return redirect(url_for('login'))
 		# compteur de visite :
 		counter_redis = redis_increment()
-
 		# valeurs retournees pour la vue
 		data = { "counter_redis" : counter_redis }
-
 		return render_template("home.html", data=data)
 
-@app.route('/login')
+@app.route('/login',methods = ['POST', 'GET'])
 def login():
-		# compteur de visite :
-		counter_redis = redis_increment()
+		if request.method == 'POST':
+			login = request.form['name']
+			# check si le login est bien dans les 3 bases puis ajout dans les 3 bases
+			neo4jGetUser(login) # neo4j
+			#TODO mongo
+			#TODO psql
+			session['logged_in'] = True
+			session['todo'] = login
+			return redirect(url_for('todo',name = login))
+		else:
+			if session.get('logged_in') :
+				return redirect(url_for('todo',name = session.get('todo')))
+			# compteur de visite :
+			counter_redis = redis_increment()
+			# valeurs retournees pour la vue
+			data = { "counter_redis" : counter_redis }
+			return render_template("login.html", data=data)
 
-		# valeurs retournees pour la vue
-		data = { "counter_redis" : counter_redis }
-		return render_template("login.html", data=data)
+@app.route('/deconnection', methods = ['POST'])
+def deconnection():
+	session['logged_in'] = False
+	session['todo'] = ""
+	return redirect(url_for('login'))
+
+@app.route('/createPostIt', methods=['POST'])
+def createpostit():
+	if not session.get('logged_in') :
+		return redirect(url_for('login'))
+	user = session.get('todo')
+	title = request.form["name"]
+	description = request.form["description"]
+	todo_date = request.form["event_date"]
+	todo_date = todo_date[6::] + todo_date[2:5] + "-" + todo_date[0:2]
+	app.logger.info(todo_date)
+	importance = request.form["importance"]
+	database_num = request.form["database"]
+
+	if databases[int(database_num)] == databases[1] :
+		conn_neo.session().write_transaction(create_post_it, user, title, todo_date, description)
+		app.logger.info("add post-it neo4j")
+	elif databases[int(database_num)] == databases[2] :
+		#do some mongo
+		app.logger.info("NON add post-it mongo")
+	else :
+		#do some psql
+		app.logger.info("NON add post-it psql")
+
+	return redirect(url_for('todo',name = user))
 
 @app.route('/redis')
 def redis():
@@ -141,4 +195,5 @@ def mongo():
 
 # Lancement de l'application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+	app.secret_key = os.urandom(12)
+	app.run(host='0.0.0.0', port=80, debug=True)
